bc01d31b112e94d83edd5a657a01b3a9e0b67a01
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2024-01-22 19:30:08 +0800

    refactor: :fire: 移除无用的宏
    现在直接使用Julia原装分派机制，其复杂度已不至使用宏来简化

--- src/Backend/Module.jl
+++ src/Backend/Module.jl
@@ -12,7 +12,6 @@ import ..NAIR: NAIR_CMD,
 # 导出
 export BackendModule
 export target_type
-export @nair_rule
 
 """
 后端模块
@@ -46,57 +45,3 @@ target_type(::BackendModule)::Type = error("未实现的`target_type`方法！")
 - ⚠可能返回空数组，表示无解析结果
 """
 function transform end =#
-
-begin "辅助开发的宏与工具函数"
-
-    """
-        @nair_rule SAV(be::BackendModule, name::String, path::String) begin
-            #= 实现 =#
-        end
-    快速定义规则
-    - 以上输入等价于：`transform(he::BackendModule, ::Val{:SAV}, name::String, path::String) = begin #= 实现 =# end`
-    - 对末尾没有用数组括起来的值（非代码块），会自动补全数组符号（:vect | `a` -> `[a]`）
-
-    已知问题：
-    - 暂不支持参数尾部注解
-    - 暂不支持where表达式
-    """
-    macro nair_rule(head::Expr, body)
-        head.head == :call || error("@nair_rule 必须使用函数调用语法！")
-
-        cmd_head::Symbol = head.args[1]
-        be_expr::Expr = head.args[2]
-        cmd_args::Vector = head.args[3:end]
-
-        call_head::Symbol = Symbol(transform)
-
-        # 自动添加数组括号（代码块除外）
-        if !(body isa Expr && body.head in [:block, :vect, :hcat, :vcat])
-            body = Expr(:vect, body)
-        end
-
-        # 构造
-        Expr(
-            :(=),
-            Expr(
-                :call,
-                call_head,
-                be_expr,
-                Expr( # `::Val{$cmd_head}`
-                    :(::),
-                    Expr(
-                        :curly,
-                        :Val,
-                        QuoteNode(cmd_head),
-                    ),
-                ),
-                cmd_args...
-            ),
-            body
-        ) |> esc
-        # :(
-        #     transform(::BackendModule, ::Val{$cmd_head}, cmd_args...) = $body
-        # )
-    end
-    
-end
